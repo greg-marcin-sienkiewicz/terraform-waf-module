b86d5eda6ad7379c499db562ffc88da8952c0fbb
Greg Sienkiewicz <greg.sienkiewicz@rewind.io>
2022-01-24 12:20:53 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "arn" {
   description = "The ARN of the WAF WebACL"
-  value       = aws_wafv2_web_acl.wafrails.arn
+  value       = aws_wafv2_web_acl.waf.arn
 }
 
 output "capacity" {
   description = "The web ACL capacity units (WCUs) currently being used by this web ACL"
-  value         = aws_wafv2_web_acl.wafrails.capacity
+  value         = aws_wafv2_web_acl.waf.capacity
 }
